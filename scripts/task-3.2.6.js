67b2ab435b9ac2556c5bd6b5e697fb174b74ddc9
PhilWaller24 <72758049+PhilWaller24@users.noreply.github.com>
2020-11-11 18:01:26 +0000

    Task-3.2.6
    Adding logical operators to temperature solution

---
+++ scripts/task-3.2.6.js
@@ -0,0 +1,32 @@
+var temperature = 30;
+
+checkTemperate(temperature);
+temperature = 50;
+checkTemperate(temperature);
+temperature = 51;
+checkTemperate(temperature);
+temperature = 99;
+checkTemperate(temperature);
+temperature = 29;
+checkTemperate(temperature);
+temperature = 33;
+checkTemperate(temperature);
+temperature = -4;
+checkTemperate(temperature);
+
+function checkTemperate() {
+
+    document.write('Hello, the temperature is ' + temperature)
+
+    if (temperature >= 50) {
+        document.write('Just pants and vest! THE TEMPERATURE IS ' + temperature + '!!!');
+
+    } else if (temperature < 0) {
+        document.write('Stay indside! THE TEMPERATURE IS ' + temperature + '!!!');
+
+    } else if (temperature > 30 && temperature < 50) {
+        document.write('Put on a coat! THE TEMPERATURE IS ' + temperature + '!!!');
+
+    } else
+        document.write('Put on a coat and hat! THE TEMPERATURE IS ' + temperature + '!!!');
+}
