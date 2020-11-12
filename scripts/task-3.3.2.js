ead8423c99ac7ca9611f9f0af662f262dff4b3cb
PhilWaller24 <72758049+PhilWaller24@users.noreply.github.com>
2020-11-12 20:38:43 +0000

    Lukes Task-3.3.2
    Added Lukes Task

--- scripts/task-3.3.2.js
+++ scripts/task-3.3.2.js
@@ -5,6 +5,10 @@ typeSelected = 'Cola';
 sizeSelected = 'Small';
 drinkSelection(typeSelected, sizeSelected);
 
+typeSelected = 'Cola';
+sizeSelected = 'Small';
+drinkOrder(typeSelected, sizeSelected);
+
 function drinkSelection(typeOfDrink, sizeOfDrink) {
 
     document.write('Hello, you have chosen a type of ' + typeOfDrink + '!');
@@ -16,20 +20,26 @@ function calculator(typeOfDrink, sizeOfDrink) {
     document.write('Hello, you have chosen a type of ' + typeOfDrink + '!');
     document.write('Hello, you have chosen a size of ' + sizeOfDrink + '!');
     document.write('You have ordered a ' + typeOfDrink + ' of ' + sizeOfDrink + '!!!');
+}
 
-    /*
-    switch (selection) {
+function drinkCaseSelection(typeOfDrink, sizeOfDrink) {
 
-        case typeOfDrink = 'Cola':
+    switch (typeOfDrink) {
+
+        case 'Cola':
 
             document.write('Hello, you have chosen a type of ' + typeOfDrink + '!');
 
             break;
-        case typeOfDrink = 'Lemonade':
+        case 'Lemonade':
+
+            document.write('Hello, you have chosen a type of ' + typeOfDrink + '!');
 
             break;
 
-        case typeOfDrink = 'Orangeade':
+        case 'Orangeade':
+
+            document.write('Hello, you have chosen a type of ' + typeOfDrink + '!');
 
             break;
 
@@ -37,5 +47,35 @@ function calculator(typeOfDrink, sizeOfDrink) {
 
 
     }
-    */
+}
+
+
+function drinkOrder(drink, size) {
+
+    // can remove case issue as follows
+    // convert to lowercase or uppercase before check
+    // for lower case use 
+    // var drinkLowerCase = drink.toLowerCase(); - but will need to use a new variable as changing value
+
+    switch (drink) {
+
+        case 'Cola':
+            //code goes here
+            document.write('Luke says, you have chosen a type of ' + size + ' ' + drink + '!');
+            break;
+        case 'Lemonade':
+            document.write('Luke says, you have chosen a type of ' + size + ' ' + drink + '!');
+            break;
+        case 'Orangeade':
+            document.write('Luke says, you have chosen a type of ' + size + ' ' + drink + '!');
+            break;
+        default:
+            //code goes here
+            document.write('Luke says, you have chosen a type of ' + size + ' ' + drink + '!');
+            document.write('Luke says, invalid type of ' + drink + '!');
+            break;
+
+    }
+
+
 }
