c870cdb696ed3fd183f276489928ec388171eef3
PhilWaller24 <72758049+PhilWaller24@users.noreply.github.com>
2020-11-24 19:53:03 +0000

    DOM task
    Use of DOM tasks examples

---
+++ scripts/events.js
@@ -0,0 +1,5 @@
+var button = document.getElementById("theImage");
+var sayHi = function(event) {
+    alert("Hi!");
+};
+button.addEventListener("click", sayHi);
---
+++ scripts/task-3.6.1.1.js
@@ -0,0 +1,15 @@
+var pageBody = document.getElementsByTagName('body')[0]
+pageBody.style.backgroundColor = 'yellow';
+pageBody.style.paddingTop = '10px';
+
+
+var warningParagraph = document.getElementById('warning');
+warningParagraph.innerHTML = 'Danger Will Robinson!';
+
+<
+button id = "myBtn" > Click Me! < /button>;
+
+var button = document.getElementById("myBtn");
+button.addEventListener("click", function(event) {
+    alert("Hi!");
+});
