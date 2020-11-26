3a5d3b8fdda4af45c4a65bf2b0aaae87519f506b
PhilWaller24 <72758049+PhilWaller24@users.noreply.github.com>
2020-11-26 16:17:42 +0000

    Events edit - Button Click Edit
    Edited events file to allow button click to change image

--- scripts/events.js
+++ scripts/events.js
@@ -1,8 +1,12 @@
-< button id = "myBtn" > Click Me! < /button>
+var btn = document.querySelector('button');
 
-var button = document.getElementById("myBtn");
-var sayHi = function(event) {
-    alert("Hi!");
-};
+btn.addEventListener('click', function(event) {
+    alert('Button Clicked');
 
-button.addEventListener("click", sayHi);
+
+    var image1 = document.querySelector('img');
+
+
+    image1.setAttribute('src', 'https://images.pexels.com/photos/5480691/pexels-photo-5480691.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500');
+
+});
